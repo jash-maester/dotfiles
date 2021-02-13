e860469923737929cb44cecda72afb5d0c5a96ee
Jashaswimalya Acharjee <jashaswimalyaacharjee@gmail.com>
2021-02-13 20:23:30 +0530

    Added a script to automate install

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
 git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
+
 curl -L https://raw.githubusercontent.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme -o .oh-my-zsh/custom/themes/bullet-train.zsh-theme
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
