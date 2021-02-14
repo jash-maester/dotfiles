aeb1587f508ca1f33fe45b5ac84d3dc63446e117
Jashaswimalya Acharjee <jashaswimalyaacharjee@gmail.com>
2021-02-14 13:06:32 +0530

    Optimised Volume script & Added install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/usr/bin/cp -rf ./ ../
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting .oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/zsh-users/zsh-autosuggestions .oh-my-zsh/custom/plugins/zsh-autosuggestions
